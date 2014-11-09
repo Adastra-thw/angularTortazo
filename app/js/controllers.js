49f0d928d4b2ab770bf1bf2a2ca4320d73cd168e
Adastra-thw <debiadastra@gmail.com>
2014-11-09 02:09:29 +0100

    Controllers and Services for 'botnet mode'.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,27 +1,43 @@
-angular.module('tortazoApp.controllers', []).
-controller('scansController', function($scope, scansService) {
-    /*$scope.scansList = [
-      {
-          id : 10,
-          scandate : 'other'
-      },
-      {
-          id : 11,
-          scandate : 'another'
-      }
-    ];*/
+var tortazoControllers = angular.module('tortazoApp.controllers', [])
 
+
+
+tortazoControllers.controller('scansController', function($scope, scansService) {
     $scope.scansList = [];
     $scope.relaysList = [];
+    /**
 
+    * Functions for "scans.html"
+    */
     scansService.getScans().success(function (response) {
         $scope.scansList = response.results;
     });
 
-    $scope.loadRelays = function(scanId) {
-      scansService.getRelays(scanId).success(function (response) {
-      $scope.relaysList = response.results;
-    });
+    $scope.functionLoadRelays = function(scanId) {
+          scansService.getRelays(scanId).success(function (response) {
+               $scope.relaysList = response.results;
+          });
     };
+})
+
+tortazoControllers.controller('botnetController', function($scope, botnetService) {
+   $scope.botsList = [];
+   $scope.botLocations = [];
+   /**
+    * Functions for "botnet.html"
+    */
+    botnetService.getBots().success(function (response) {
+        $scope.botsList  = response.results;
+    });
+
+    $scope.functionBotByNickName = function(nickName) {
+        scansService.getBotByNickName(nickName).success(function (response) {
+            $scope.botsList  = response.results;
+        })
+    };
+
+    botnetService.getBotLocations().success(function (response) {
+        $scope.botLocations  = response.results;
+    });
 
 })
--- app/js/services.js
+++ app/js/services.js
@@ -1,20 +1,43 @@
-angular.module('tortazoApp.services', []).
-  factory('scansService', function($http) {
+var tortazoApp = angular.module('tortazoApp.services' , []);
 
-    var djangoTortazoAPI = {};
-    djangoTortazoAPI.getScans = function() {
+tortazoApp.factory('scansService', function($http) {
+
+    var apiScans = {};
+    apiScans.getScans = function() {
       return $http({
         url: 'http://127.0.0.1:8000/scans'
       });
     }
 
 
-    djangoTortazoAPI.getRelays = function(scanId) {
+    apiScans.getRelays = function(scanId) {
       return $http({
         url: 'http://127.0.0.1:8000/scan/torNodeData/?scanid='+scanId
       });
     }
+    return apiScans;
+})
 
+tortazoApp.factory('botnetService', function($http) {
 
-    return djangoTortazoAPI;
+    var apiBotnet = {};
+    apiBotnet.getBots = function() {
+      return $http({
+        url: 'http://127.0.0.1:8000/botnet/bots/'
+      });
+    }
+
+    apiBotnet.getBotByNickName = function(nickname) {
+      return $http({
+        url: 'http://127.0.0.1:8000/botnet/bots/?nickname='+nickname
+      });
+    }
+
+    apiBotnet.getBotLocations = function(nickname) {
+      return $http({
+        url: 'http://127.0.0.1:8000/botnet/locations/'
+      });
+    }
+
+    return apiBotnet;
   });
