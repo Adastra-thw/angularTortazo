37a3c247b532ae4b05ef04c20b179f7674456fe9
Adastra-thw <debiadastra@gmail.com>
2014-12-27 01:20:55 +0100

    Templating and routes

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,27 @@
-angular.module('tortazoApp', [
+var app = angular.module('tortazoApp', [
+  'ngRoute',
+  'gettext',
   'tortazoApp.controllers',
   'tortazoApp.ui',
   'tortazoApp.services'
 ]);
+
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/scans', {
+            templateUrl: 'scans.html',
+            controller: 'scansController'
+        }).
+        when('/onionrepo', {
+            templateUrl: 'onionrepository.html',
+            controller: 'onionRepoController'
+        }).
+        when('/botnet', {
+            templateUrl: 'botnet.html',
+            controller: 'botnetController'
+        }).
+        otherwise({
+            redirectTo: '/'
+      });
+    }
+    ]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -93,19 +93,19 @@ bootstrapModule.controller('IndexAccordion', function ($scope) {
     {
       title: 'Gather Information.',
       content: 'This is the simplest mode of execution in Tortazo. In this mode, Tortazo will perform an Nmap scan and the results for every exit node in the Directory Authorities or in the local TOR instance will be stored in the local database used by Tortazo.',
-      urlLink: 'scans.html',
+      urlLink: '/tortazoWeb/#scans',
       textLink: 'Check the gathered data in this Tortazo instance!'
     },
     {
       title: 'Onion Repository.',
       content: 'Tortazo will try to generate ONION addresses and then tests if the generated addresses point to a hidden service in the deep web',
-      urlLink: 'onionrepository.html',
+      urlLink: '/tortazoWeb/#onionrepo',
       textLink: 'Check the Onion Repository data in this Tortazo instance!'
     },
     {
       title: 'Botnet Mode.',
       content: 'In this mode, Tortazo will read the file "TORTAZO_DIR/tortazo_botnet.bot" to create the bots in the context of the botnet. You can run parallel commands against the entirely botnet or exclude bots to run the commands just over some machines.',
-      urlLink: 'botnet.html',
+      urlLink: '/tortazoWeb/#botnet',
       textLink: 'Check the botnet data in this Tortazo instance!'
     },
     {
@@ -118,5 +118,47 @@ bootstrapModule.controller('IndexAccordion', function ($scope) {
   $scope.status = {
     isFirstOpen: true,
     isFirstDisabled: false
+  };
+});
+
+bootstrapModule.controller('MainModalController', function ($scope, $modal, $log) {
+  //$scope.items = ['item1', 'item2', 'item3'];
+  $scope.open = function (size) {
+    var modalInstance = $modal.open({
+      templateUrl: 'templateModal.html',
+      controller: 'templateModalController',
+      size: size/*,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }*/
+    });
+
+    /*modalInstance.result.then(function (selectedItem) {
+      //$scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });*/
+  };
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+bootstrapModule.controller('templateModalController', function ($scope, $modalInstance){ //, items) {
+
+  /*$scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };*/
+
+  $scope.ok = function () {
+    //$modalInstance.close($scope.selected.item);
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
   };
 });
